34646aad1d98da1b311128e3f7b940ff41bcdb92
scramblerdoodle <orlando.pedro@outlook.com>
2022-01-05 22:58:46 +0100

    functions

--- Multimedia.cpp
+++ Multimedia.cpp
@@ -1,1 +1,18 @@
 #include "Multimedia.h"
+
+using namespace std;
+
+// Constructor
+Multimedia::Multimedia(string name, string pathname) {
+    setName(name);
+    setPath(pathname);
+};
+
+// Public functions
+void Multimedia::setName(string name) {
+    m_name = name;    
+};
+
+void Multimedia::setPath(string pathname) {
+    m_pathname = pathname;
+}
