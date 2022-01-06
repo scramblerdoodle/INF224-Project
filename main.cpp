d21a8a56be962d23fcef2432fbcbecad12f67647
scramblerdoodle <orlando.pedro@outlook.com>
2022-01-06 02:08:45 +0100

    tests for video play and image open

--- main.cpp
+++ main.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
+
 #include "Video.h"
-#include "Multimedia.h"
+#include "Image.h"
 using namespace std;
 
 int main(int argc, const char* argv[])
 {
-    Video * video = new Video("customname", "custompath", 600);
-    Video * videoDefault = new Video();
+    Video * video = new Video("skeleton", "/media/orlando/Files/Videos/webm/attack helicopter invades peaceful skeleton realm.mp4", 600);
+    Image * image = new Image("jolly", "/media/orlando/Files/Pictures/157899859_284324719767687_2825453359009430038_n.jpg", 500, 500);
     
-    Multimedia * media = new Multimedia();
-    cout << "Multimedia" << endl;
-    media->print(cout);
-    cout << endl;
-
     video->print(cout);
-    videoDefault->print(cout);
-
+    video->play();
+    
+    image->print(cout);
+    image->open();
 
     return 0;
 }
