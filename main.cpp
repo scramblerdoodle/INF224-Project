d0d48ba94181db409f81a11d876cc66fd953a627
scramblerdoodle <orlando.pedro@outlook.com>
2022-01-10 18:55:12 +0100

    Silly changes

--- main.cpp
+++ main.cpp
@@ -18,10 +18,22 @@ int main(int argc, const char* argv[])
 
 
     GroupPtr gr = objects.getGroup("group");
-    FilmPtr film(new Film("skeleton", "/media/orlando/Files/Videos/webm/attack helicopter invades peaceful skeleton realm.mp4", 23, chaptersSkeleton));
-    ImagePtr image(new Image("jolly", "/media/orlando/Files/Pictures/157899859_284324719767687_2825453359009430038_n.jpg", 500, 500));
-    gr->push_back(film);
-    gr->push_back(image);
+    gr->push_back(
+        FilmPtr (
+            new Film(
+                "skeleton", "/media/orlando/Files/Videos/webm/attack helicopter invades peaceful skeleton realm.mp4", 
+                23, chaptersSkeleton
+            )
+        )
+    );
+    gr->push_back(
+        ImagePtr(
+            new Image(
+                "jolly", "/media/orlando/Files/Pictures/157899859_284324719767687_2825453359009430038_n.jpg", 
+                500, 500
+            )
+        )
+    );
 
     delete [] chaptersSkeleton;
 
