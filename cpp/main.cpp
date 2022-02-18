a89ebc8569e8bb7d1af1d3d9c09474d7935dd114
scramblerdoodle <orlando.pedro@outlook.com>
2022-02-18 15:19:48 +0100

    i have no idea what i'm doing but hey im testing request opts

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -38,94 +38,112 @@ static void Initialize()
     s_mapOptionValues["q"] = evEnd;
 }
 
+static void processRequest(string request, Map objects, ostream& output);
+
 
 int main(int argc, const char* argv[])
 {    
-
     Map objects("../media.txt");
 
-    string line, word;
-    vector<string> opts;
+    string request;
+    stringstream output;
 
     mediaDict medias;
     groupDict groups;
     
     Initialize();
-    cout << endl << "Type 'help' to list all available commands." << endl;
+    std::cout << endl << "Type 'help' to list all available commands." << endl;
     
     while (1)
     {
-        opts = {};
+        output = {};
+        std::cout << "Choose a command: ";
+        getline(cin, request);
+        processRequest(request, objects, output);
 
-        cout << "Choose a command: ";
-        getline(cin, line);
-        stringstream X (line);    
-        while ( getline(X, word, ' ') )
-            opts.push_back(word);
-
-        switch (s_mapOptionValues[opts[0]])
+        std::cout << output.str();
+        if (output.str() == "quit") 
         {
-        case evHelp:
-            cout << "help: list commands" << endl;
-            cout << "nlist: show all media options" << endl;
-            cout << "print <option>: show info related to a chosen media file" << endl;
-            cout << "play <option>: play a media file" << endl;
-            cout << "search <pattern>: look for media starting with chosen pattern" << endl;
-            cout << "add <type, ...>" << endl;
-            cout << "remove <option>" << endl;
-            cout << "edit <option>" << endl;
-            cout << "save" << endl;
-            cout << "quit/q" << endl;
-            break;
-
-        case evList:
-            medias = objects.getAllMedias();
-            cout << "Medias:" << endl;
-            for (auto it : medias)
-                cout << it.first << endl;
-            
-            groups = objects.getAllGroups();
-            cout << endl << "Groups:" << endl;
-            for (auto it : groups)
-                cout << it.first << endl;
-
-            break;
-
-        case evPrint:
-            objects.print(opts[1], cout);
-            break;
-
-        case evPlay:
-            objects.play(opts[1]);
-            break;
-
-        case evSearch:
-            cout << "[Code to search media]" << endl;
-            break;
-
-        case evAdd:
-            cout << "[Code to add new media to obj]" << endl;
-            break;
-
-        case evRemove:
-            cout << "[Code to remove some media from obj]" << endl;
-            break;
-
-        case evEdit:
-            cout << "[Code to edit some item in the obj]" << endl;
-            break;
-
-        case evSave:
-            cout << "[Code to save the obj into the .txt file]" << endl;
-            break;
-
-        case evEnd:
-            return 0;
-
-        default:
-            cout << "Command " << opts[0] << " not found." << endl;
+            std::cout << '\n';
             break;
         }
-
     }
+
+    return 0;
+}
+
+void processRequest(string request, Map objects, ostream& output)
+{
+    string word;
+    vector<string> opts{};
+
+    if (request.empty()) opts = {""};
+
+    stringstream X (request);    
+    while ( getline(X, word, ' ') )
+        opts.push_back(word);
+
+    switch (s_mapOptionValues[opts[0]])
+    {
+    case evHelp:
+        output << "help: list commands" << endl;
+        output << "nlist: show all media options" << endl;
+        output << "print <option>: show info related to a chosen media file" << endl;
+        output << "play <option>: play a media file" << endl;
+        output << "search <pattern>: look for media starting with chosen pattern" << endl;
+        output << "add <type, ...>" << endl;
+        output << "remove <option>" << endl;
+        output << "edit <option>" << endl;
+        output << "save" << endl;
+        output << "quit/q" << endl;
+        break;
+
+    case evList:
+        output << "Medias:" << endl;
+        for (auto it : objects.getAllMedias())
+            output << it.first << endl;
+        
+        output << endl << "Groups:" << endl;
+        for (auto it : objects.getAllGroups())
+            output << it.first << endl;
+
+        break;
+
+    case evPrint:
+        objects.print(opts[1], output);
+        break;
+
+    case evPlay:
+        objects.play(opts[1]);
+        break;
+
+    case evSearch:
+        output << "[Code to search media]" << endl;
+        break;
+
+    case evAdd:
+        output << "[Code to add new media to obj]" << endl;
+        break;
+
+    case evRemove:
+        output << "[Code to remove some media from obj]" << endl;
+        break;
+
+    case evEdit:
+        output << "[Code to edit some item in the obj]" << endl;
+        break;
+
+    case evSave:
+        output << "[Code to save the obj into the .txt file]" << endl;
+        break;
+
+    case evEnd:
+        output << "quit";
+        break;
+
+    default:
+        output << "Command " << opts[0] << " not found." << endl;
+        break;
+    }
+
 }
